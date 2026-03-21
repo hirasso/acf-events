d87a4ec0ddafb832721765912f6891e137185e8d
Rasso Hilber <mail@rassohilber.com>
2026-03-21 11:13:01 +0100

    cleanup

--- src/Core.php
+++ src/Core.php
@@ -43,7 +43,7 @@ final class Core
         add_filter('relevanssi_post_title_before_tokenize', [$this, 'relevanssi_post_title_before_tokenize'], 10, 2);
         add_filter('pll_get_post_types', [$this, 'pll_get_post_types'], 10, 2);
         add_filter('query_vars', [$this, 'query_vars']);
-        add_action('pre_get_posts', [$this, 'prepare_archive_main_query']);
+        add_action('pre_get_posts', $this->prepare_main_query(...));
         add_filter('term_link', [$this, 'term_link'], 10, 2);
         add_filter('relevanssi_hits_filter', [$this, 'relevanssi_hits_filter'], 10, 2);
         add_action('restrict_manage_posts', $this->renderYearFilter(...));
@@ -320,9 +320,13 @@ final class Core
     /**
      * Prepare the main archive query for events
      */
-    public function prepare_archive_main_query(WP_Query $query): void
+    public function prepare_main_query(WP_Query $query): void
     {
-        if (!$query->is_main_query() || !$query->is_archive()) {
+        if (!$query->is_main_query()) {
+            return;
+        }
+
+        if (!$query->is_archive()) {
             return;
         }
 
